6094c068fee1a526b9fcfbe8d1a8f9f47b441955
Mynk-9 <Mynk-9@users.noreply.github.com>
2021-01-10 01:54:11 +0530

    Added MainPage section and albumcard

--- beautplayer/src/pages/mainpage/MainPage.js
+++ beautplayer/src/pages/mainpage/MainPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import PlayerBar from './../../components/playerbar/PlayerBar';
+import AlbumCard from './../../components/albumcard/AlbumCard';
+import './../../components/commonstyles.scss';
 import Styles from './MainPage.module.scss';
+
+import AlbumArt from './../../assets/images/pexels-sebastian-palomino-1955134.jpg';
 
 export default class MainPage extends React.Component {
     constructor() {
@@ -21,7 +25,14 @@ export default class MainPage extends React.Component {
             <div id="color-mode-setter" className={this.state.darkMode}>
                 <Navbar />
                 <div className={Styles.mainBody}>
-
+                    <div className={Styles.section}>
+                        <div className={Styles.sectionHead}>
+                            Section Head
+                        </div>
+                        <div className={Styles.sectionBody}>
+                            <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        </div>
+                    </div>
                 </div>
                 <PlayerBar />
             </div>
