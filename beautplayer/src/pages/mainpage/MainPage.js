a633e0fd51158f58a1c2cfa3abf8b67f48693477
Mynk-9 <Mynk-9@users.noreply.github.com>
2021-01-29 12:07:08 +0530

    Changed MainPage to functional component and added a non scrolling section body component

--- beautplayer/src/pages/mainpage/MainPage.js
+++ beautplayer/src/pages/mainpage/MainPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import PlayerBar from './../../components/playerbar/PlayerBar';
 import AlbumCard from './../../components/albumcard/AlbumCard';
@@ -7,57 +7,61 @@ import Styles from './MainPage.module.scss';
 
 import AlbumArt from './../../assets/images/pexels-steve-johnson-1234853.jpg';
 
-export default class MainPage extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            acrylicColor: '--acrylic-color',
-        };
-    }
+const MainPage = (props) => {
+    const [acrylicColor, setAcrylicColor] = useState('--acrylic-color');
 
-    render() {
-        return (
-            <div>
-                <Navbar
-                    acrylicColor={this.state.acrylicColor}
-                />
-                <div className={Styles.mainBody}>
-                    <div className={Styles.section}>
-                        <div className={Styles.sectionHead}>
-                            Playlist
-                        </div>
-                        <div className={Styles.sectionBody}>
-                            <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
-                            <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
-                            <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
-                            <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
-                            <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
-                        </div>
-                    </div>
-                    <div className={Styles.section}>
-                        <div className={Styles.sectionHead}>
-                            Playlist
-                        </div>
-                        <div className={Styles.sectionBody}>
-                            <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
-                        </div>
-                    </div>
-                    <div className={Styles.section}>
-                        <div className={Styles.sectionHead}>
-                            Playlist
-                        </div>
-                        <div className={Styles.sectionBody}>
-                            <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
-                        </div>
+    return (
+        <div>
+            <Navbar
+                acrylicColor={this.state.acrylicColor}
+            />
+            <div className={Styles.mainBody}>
+                <div className={Styles.section}>
+                    <div className={Styles.sectionHead}>Playlists</div>
+                    <div className={Styles.sectionBody}>
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
                     </div>
                 </div>
-                <PlayerBar
-                    acrylicColor={this.state.acrylicColor}
-                    albumArt={AlbumArt}
-                    AlbumTitle="Awesome Album"
-                    albumArtist="Human"
-                />
+                <div className={Styles.section}>
+                    <div className={Styles.sectionHead}>All Albums</div>
+                    <div className={`${Styles.sectionBody} ${Styles.sectionBodyNoScroll}`}>
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                        <AlbumCard albumArt={AlbumArt} albumTitle={"Awesome Album"} albumArtist={"Human"} />
+                    </div>
+                </div>
             </div>
-        );
-    }
+            <PlayerBar
+                acrylicColor={this.state.acrylicColor}
+                albumArt={AlbumArt}
+                AlbumTitle="Awesome Album"
+                albumArtist="Human"
+            />
+        </div>
+    );
 }
+
+
+export default MainPage;
