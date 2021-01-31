5b0fb2183477a21bf4cd0390e9aa3c9ab425c3f7
Mynk-9 <Mynk-9@users.noreply.github.com>
2021-02-01 02:08:39 +0530

    Streaming route of tracks added

--- mediaserver/api/routes/tracks.js
+++ mediaserver/api/routes/tracks.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const Tracks = require('./../models/tracks');
@@ -26,6 +27,60 @@ router.get('/:trackId', (req, res, next) => {
             res.status(200).json({
                 Track: track
             });
+        })
+        .catch(e => {
+            console.log(e);
+            res.status(500).json({
+                error: e
+            });
+        });
+});
+
+// for streaming
+router.get('/:trackId/stream', (req, res, next) => {
+    const id = req.params.trackId;
+    Tracks.findById(id)
+        .then(track => {
+
+            // code analysed picked from https://gist.github.com/DingGGu/8144a2b96075deaf1bac
+
+            let music = '';
+            music = track['path'];
+
+            let stat = fs.statSync(music);
+            range = req.headers.range;
+            let readStream;
+
+            if (range !== undefined) {
+                let parts = range.replace(/bytes=/, "").split("-");
+
+                let partial_start = parts[0];
+                let partial_end = parts[1];
+
+                if ((isNaN(partial_start) && partial_start.length > 1) || (isNaN(partial_end) && partial_end.length > 1)) {
+                    return res.sendStatus(500); //ERR_INCOMPLETE_CHUNKED_ENCODING
+                }
+
+                let start = parseInt(partial_start, 10);
+                let end = partial_end ? parseInt(partial_end, 10) : stat.size - 1;
+                let content_length = (end - start) + 1;
+
+                res.status(206).header({
+                    'Content-Type': 'audio/mpeg',
+                    'Content-Length': content_length,
+                    'Content-Range': "bytes " + start + "-" + end + "/" + stat.size
+                });
+
+                readStream = fs.createReadStream(music, { start: start, end: end });
+            } else {
+                res.header({
+                    'Content-Type': 'audio/mpeg',
+                    'Content-Length': stat.size
+                });
+                readStream = fs.createReadStream(music);
+            }
+            readStream.pipe(res);
+
         })
         .catch(e => {
             console.log(e);
