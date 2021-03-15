66a13a4e3cc19695c990b370728294a7aea71694
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2021-03-16 03:21:48 +0530

    Added option to remove track from queue

--- beautplayer/src/pages/playerqueue/PlayerQueue.js
+++ beautplayer/src/pages/playerqueue/PlayerQueue.js
@@ -11,12 +11,14 @@ import ThemeContext from './../../components/themecontext';
 import PlayerContext from './../../components/playercontext';
 
 import LeftIcon from './../../assets/buttonsvg/chevron-left.svg';
+import MinusIcon from './../../assets/buttonsvg/minus.svg';
 
 const PlayerQueue = () => {
     const [acrylicColorStyle, setAcrylicColorStyle] = useState({});
     const [tableAcrylicColorStyle, setTableAcrylicColorStyle] = useState({});
+    const [trackList, setTrackList] = useState([]);
     const { acrylicColor, letAcrylicTints } = useContext(ThemeContext);
-    const { playerQueue } = useContext(PlayerContext);
+    const { playerQueue, setPlayerQueue } = useContext(PlayerContext);
 
     useEffect(() => {
         if (!letAcrylicTints) {
@@ -37,35 +39,57 @@ const PlayerQueue = () => {
 
     let history = useHistory();
 
-    let key = 0;
-    let trackList = playerQueue.map((data) => {
-        ++key;
-        return (
-            <tr key={key} className={Styles.trackEntry}>
-                <td><TrackLiker trackId={data.trackId} /></td>
-                <td>
-                    <PlayButton
-                        audioSrc={data.audioSrc}
-                        audioDuration={data.audioDuration}
-                        track={data.track}
-                        albumArt={data.albumArt}
-                        albumTitle={data.albumTitle}
-                        albumArtist={data.albumArtist}
-                        isPlaylist={data.isPlaylist}
-                        playlistTitle={data.playlistTitle}
-                        addToQueue={() => { }}  // already in the queue
-                    />
-                </td>
-                <td>{data.track}</td>
-                <td>{data.albumArtist}</td>
-                <td>{data.audioDuration}</td>
-            </tr>
-        );
+    let removeItem = (trackId) => {
+        let queue = playerQueue;
+        let i = 0;
+        for (; i < playerQueue.length; ++i)
+            if (trackId === queue[i].trackId)
+                break;
+        queue.splice(i, 1);
+        setPlayerQueue(queue);
+    };
+
+    useEffect(() => {
+        let key = 0;
+        let _trackList = playerQueue.map((data) => {
+            ++key;
+            return (
+                <tr key={key} className={Styles.trackEntry}>
+                    <td><TrackLiker trackId={data.trackId} /></td>
+                    <td>
+                        <PlayButton
+                            audioSrc={data.audioSrc}
+                            audioDuration={data.audioDuration}
+                            track={data.track}
+                            albumArt={data.albumArt}
+                            albumTitle={data.albumTitle}
+                            albumArtist={data.albumArtist}
+                            isPlaylist={data.isPlaylist}
+                            playlistTitle={data.playlistTitle}
+                            addToQueue={() => { }}  // already in the queue
+                        />
+                    </td>
+                    <td>{data.track}</td>
+                    <td>{data.albumArtist}</td>
+                    <td>{data.audioDuration}</td>
+                    <td>
+                        <img
+                            data-dark-mode-compatible
+                            src={MinusIcon}
+                            onClick={() => removeItem(data.trackId)}
+                            alt={"Remove"}
+                        />
+                    </td>
+                </tr>
+            );
+        });
+        // oldest track would be at the bottom
+        // playing tracks would advance from down
+        // to top
+        _trackList.reverse();
+
+        setTrackList(_trackList);
     });
-    // oldest track would be at the bottom
-    // playing tracks would advance from down
-    // to top
-    trackList.reverse();
 
     return (
         <>
