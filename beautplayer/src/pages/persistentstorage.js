3aa4e0e2af9b9e3550401a1c9a946a30d53b64be
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2021-04-30 08:44:04 +0530

    Main-page albums sorting now persists across sessions

--- beautplayer/src/pages/persistentstorage.js
+++ beautplayer/src/pages/persistentstorage.js
@@ -2,7 +2,7 @@ class PersistentStorage {
     static MainPageAllAlbumCards = [];
     static MainPagePlaylistCards = [];
     static MainPageActivePage = "albums"; // albums, playlists
-    static MainPageAlbumsSort = "name";   // sortBy value
+    static MainPageAlbumsSort = null;   // sortBy value
 }
 
 export default PersistentStorage;
