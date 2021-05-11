b586243658b3d22bf2ebee07e98bc4c455bc8474
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2021-05-11 12:07:33 +0530

    Created BeautPlayerQueue

---
+++ beautplayer/src/components/queue.js
@@ -0,0 +1,21 @@
+var BeautPlayerQueue = [];
+
+// see components/tracklist/TrackList.js for more info
+// [
+//     {
+//         'trackId': ...,
+//         'audioSrc': ...,
+//         'audioDuration': ...,
+//         'track': ...,
+//         'albumArt': ...,
+//         'albumTitle': ...,
+//         'albumArtist': ...,
+//         'isPlaylist': ...,
+//         'playlistTitle': ...,
+//         'linkBack': ...
+//     },
+//     ...
+// ]
+
+
+export default BeautPlayerQueue;
