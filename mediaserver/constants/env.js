76ebdf3ad52b351f25c1ed97e8dbd252c74db41b
Mayank Mathur <Mynk-9@users.noreply.github.com>
2023-05-06 22:30:34 +0530

    Added AWS S3 requirements

---
+++ mediaserver/constants/env.js
@@ -0,0 +1,11 @@
+const SERVER_PORT = process.env.SERVER_PORT;
+const AWS_REGION = process.env.AWS_REGION;
+const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+const AWS_BUCKET_SERVER = process.env.AWS_BUCKET_SERVER;
+
+module.exports = {
+  SERVER_PORT,
+  AWS_BUCKET_NAME,
+  AWS_REGION,
+  AWS_BUCKET_SERVER,
+};
