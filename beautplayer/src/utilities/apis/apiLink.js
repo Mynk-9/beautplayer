0bc0374ebbae3a3f15e39833cefca00331feedac
Mayank Mathur <Mynk-9@users.noreply.github.com>
2023-09-18 20:21:10 +0530

    updated api link for FE

--- beautplayer/src/utilities/apis/apiLink.js
+++ beautplayer/src/utilities/apis/apiLink.js
@@ -1,9 +1,6 @@
 const API =
     process.env.NODE_ENV === 'development'
-        ? window.location.origin.substring(
-              0,
-              window.location.origin.lastIndexOf(':')
-          ) + ':5000'
+        ? process.env.REACT_APP_API_DEV
         : process.env.REACT_APP_API_PROD;
 
 export default API;
